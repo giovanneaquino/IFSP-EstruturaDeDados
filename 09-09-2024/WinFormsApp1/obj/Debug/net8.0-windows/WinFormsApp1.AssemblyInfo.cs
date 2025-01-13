bbe436a0eb17152a29768c19365c1302820f97a0
giovanneaquino <giovannebrandao13@gmail.com>
2025-01-13 17:43:04 -0300

    Fix path name

--- 09-09-2024/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ 09-09-2024/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801687ba77544e2d612d68a1f69783f41939c204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae04b0eeaf9c3f874f0554243ef5c038d3ab0e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++
@@ -1,3 +0,0 @@
-Alunos: Giovanne Brandão de Aquino(CB3026591) e Ricardo Queiroz Oliani(CB3025543)
-Turma: ADS371
-
